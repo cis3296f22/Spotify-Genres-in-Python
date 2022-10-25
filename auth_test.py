f85527208d2784308125c62e1d7637a69fb4a6a4
Jason Michel <tuk94727@temple.edu>
2022-10-25 08:58:05 -0400

    Jason Compiled

--- auth_test.py
+++ auth_test.py
@@ -5,10 +5,11 @@ from spotipy.oauth2 import SpotifyOAuth
 scope = "user-library-read"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id="c6f354662a924f81b9af0aa615832c2b", 
-    client_secret="6127244cbcc64eaa8a20959bb161c7bb", 
+    client_id="c6f354662a924f81b9af0aa615832c2b",
+    client_secret="6127244cbcc64eaa8a20959bb161c7bb",
     redirect_uri="http://localhost", scope=scope,))
 
+# Jason Compiled
 # When running, browser will open a login page to Spotify. Login to Spotify.
 # You will be redirected to a localhost URL. Copy this URL and paste it into the
 # terminal for this program when prompted.
