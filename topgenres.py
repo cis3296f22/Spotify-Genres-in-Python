0b4d9309f90fb323b9a90a3399613b573bff78e5
joshdeland <tuo07755@temple.edu>
2022-10-31 11:59:45 -0400

    add genre frequency function and dictionary

--- topgenres.py
+++ topgenres.py
@@ -1,15 +1,30 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+def main():
 
-scopes = ["user-library-read", "user-top-read"]
+    scopes = ["user-library-read", "user-top-read"]
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id="c6f354662a924f81b9af0aa615832c2b",
-    client_secret="6127244cbcc64eaa8a20959bb161c7bb",
-    redirect_uri="http://localhost", scope=scopes,))
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id="c6f354662a924f81b9af0aa615832c2b",
+        client_secret="6127244cbcc64eaa8a20959bb161c7bb",
+        redirect_uri="http://localhost", scope=scopes,))
 
-top_artists = sp.current_user_top_artists(20, 0, "short_term")
+    top_artists = sp.current_user_top_artists(20, 0, "short_term")
 
-for index, artist in enumerate(top_artists["items"]):
-    print(index, artist["genres"])
+    genreDict = {}
+    for index, artist in enumerate(top_artists["items"]):
+        print(index, artist["genres"])
+        for genre in artist["genres"]:
+            get_genre_frequency(artist["genres"], genreDict)
+    print(genreDict)
+    
+def get_genre_frequency(genres, genreDict): 
+    for genre in genres:
+        if genreDict[genre]:
+            genreDict[genre] += 1
+        else: 
+            genreDict[genre] = 1
+    return genreDict
+
+main()
