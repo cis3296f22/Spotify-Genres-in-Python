74449c695dc1c445812a8baff211c86eac01edff
joshdeland <tuo07755@temple.edu>
2022-10-31 21:48:41 -0400

    Sorted genres in list

--- topgenres.py
+++ topgenres.py
@@ -11,23 +11,26 @@ def main():
         redirect_uri="http://localhost", scope=scopes,))
 
     top_artists = sp.current_user_top_artists(20, 0, "short_term")
-
+    
+    # Create empty dictionary, put in artists and get frequency of genres from artists 
     genreDict = {}
     for artist in top_artists["items"]:
-        print(artist["name"],  artist["genres"])
         get_genre_frequency(artist["genres"], genreDict)
         
-           
-    print(genreDict)
-    # This part doesnt work right now
-    sorted_genres = sorted(genreDict.values)
-    sorted_dict = {}
+    # Sort genre count from highest to lowest frequency
+    sorted_genres = sorted(genreDict.items(), reverse = True, key=lambda kv:
+        (kv[1], kv[0]))
+    
+    ''' sorted_dict = {}
     for val in sorted_genres:
        for key in genreDict.keys():
            if genreDict[key] == val:
-               sorted_dict[key] = genreDict[key]
-    print(sorted_dict) 
+               sorted_dict[key] = genreDict[key]'''
     
+    # Print sorted list
+    print(sorted_genres) 
+    
+# Increment value in dictionary every time genre is found
 def get_genre_frequency(genres, genreDict):
 
     for genre in genres:
