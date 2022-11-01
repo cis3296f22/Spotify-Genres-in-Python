5390731beac8629bd0d12331acdefd321b468606
joshdeland <tuo07755@temple.edu>
2022-11-01 07:09:40 -0400

    added host

--- spotifygenres/settings.py
+++ spotifygenres/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-n7*5lkwxc7fl%_+c1vm_rd%d6wm4s1ek6-qcs+=h3tr$^*+v6(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['spotify-genres-in-python.herokuapp.com']
 
 
 # Application definition
