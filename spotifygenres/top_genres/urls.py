4c9e3eee8fc452ae504837c613cfbcc565bfb556
joshdeland <tuo07755@temple.edu>
2022-11-08 08:15:33 -0500

    Remove redundant folders

--- spotifygenres/top_genres/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    #path('', views.login_view, name='login_view'),
-    path('', views.top_genre_view, name='top_genre_view'),
-]
